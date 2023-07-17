7b7d235bd0e85988562656df8fa175df4ccdbde1
DenisSher <47759876+DenisSher@users.noreply.github.com>
2023-07-17 16:41:20 +0300

    Add files via upload

--- pythonProject/urls.py
+++ pythonProject/urls.py
@@ -24,19 +24,35 @@ from interface.views import index, monitor, info, \
      GraphAgentDataAdd, AgentErrorsDeleteData
 
 urlpatterns = [
+    # Главная страница
     path('', index),
+    # Страница мониторинга
     path('monitor/', monitor),
+    # Страница информации
     path('info/', info),
+    # Страница администратора
     path('admin/', admin.site.urls),
+    # API для добавления данных агента
     path('api/addAgentData', AgentAddData.as_view()),
+    # API для редактирования данных агента с использованием его идентификатора (pk)
     path('api/editAgentData/<int:pk>/', AgentEditData.as_view(), name='agent-edit'),
+    # API для удаления данных агента с использованием его идентификатора (pk)
     path('api/deleteAgentData/<int:pk>/', AgentDeleteData.as_view(), name='agent-delete'),
+    # API для добавления данных группы
     path('api/addGroupData', GroupAddData.as_view(), name='group-add'),
+    # API для редактирования данных группы с использованием ее идентификатора (pk)
     path('api/editGroupData/<int:pk>/', GroupEditData.as_view(), name='group-edit'),
+    # API для удаления данных группы с использованием ее идентификатора (pk)
     path('api/deleteGroupData/<int:pk>/', GroupDeleteData.as_view(), name='group-delete'),
-    path('monitor/api/deleteAgentErrorsData/<int:agent_id>/', AgentErrorsDeleteData.as_view(), name='agents-errors-delete'),
+    # API для удаления данных об ошибках агента с использованием его идентификатора (agent_id)
+    path('monitor/api/deleteAgentErrorsData/<int:agent_id>/', AgentErrorsDeleteData.as_view(),
+         name='agents-errors-delete'),
+    # API для получения данных для графика
     path('api/graphData/', GraphApiData.as_view(), name='graph-data'),
+    # API для отправки запроса агенту
     path('api/SendRequestToAgent', SendRequestToAgent.as_view(), name='graph-data'),
+    # API для синхронизации данных агента
     path('api/syncAgentData', SyncAgentData.as_view(), name='agent-sync'),
+    # API для добавления данных агента для графика
     path('api/graphAgentDataAdd', GraphAgentDataAdd.as_view(), name='graph-add-data'),
 ]
