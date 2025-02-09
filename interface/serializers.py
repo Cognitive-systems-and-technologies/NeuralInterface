dc16453634c131c41261e6ba224107990d07f7f4
DenisSher <47759876+DenisSher@users.noreply.github.com>
2025-02-09 15:10:39 +0300

    Update serializers.py

--- interface/serializers.py
+++ interface/serializers.py
@@ -5,31 +5,34 @@ from .models import Agents, AgentGroups, AgentTypes, AgentErrorsView, NeuralAlgo
 
 
 # Сериализаторы для агентов
+# Сериализатор для добавления агента
 class AgentAddSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agents
         fields = ('agent_name', 'agent_group_id', 'agent_status', 'agent_description', 'datetime_create',
                   'agent_type_id', 'agent_mac_address', 'agent_ip_address', 'agent_port')
 
-
+# Сериализатор для изменения агента
 class AgentEditSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agents
         fields = '__all__'
 
 
+# Сериализатор для удаления агента
 class AgentDeleteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agents
         fields = '__all__'
 
 
+# Сериализатор для изменения агента
 class NeuralAlgorithmsSerializer(serializers.ModelSerializer):
     class Meta:
         model = NeuralAlgorithms
         fields = '__all__'
 
-
+# Сериализатор для обработки ошибок
 class AgentErrorsSerializer(serializers.ModelSerializer):
     class Meta:
         model = AgentErrors
